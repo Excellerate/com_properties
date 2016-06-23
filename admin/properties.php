ea30ad72dba98b0dd93aba0a121af6c2f06590ce
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2016-06-23 12:59:00 +0200

    A few extra files

--- admin/properties.php
+++ admin/properties.php
@@ -1,1 +1,10 @@
-Hello Admin Properties
+<?php
+defined('_JEXEC') or die;
+
+if (!JFactory::getUser()->authorise('core.manage', 'com_properties')){
+  return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+
+$controller  = JControllerLegacy::getInstance('Properties');
+$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->redirect();
